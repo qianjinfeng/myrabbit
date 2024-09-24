8493a9e9c56362e4c66804b593003912f2222ee3
jinfeng <qianjinfeng@outlook.com>
2024-09-24 21:24:58 +0800

    add elastic mapping files

--- Readme.md
+++ Readme.md
@@ -1,2 +1,5 @@
 
 https://www.dicomlibrary.com/dicom/dicom-tags/
+
+https://dicom.innolitics.com/ciods/ct-image/
+
--- index.js
+++ index.js
@@ -4,7 +4,16 @@ import  arrayItem  from "./test/arrayItem.json" assert { type: "json" };
 import { DicomMetaDictionary } from "./src/DicomMetaDictionary.js";
 import { log } from "./src/log.js";
 import { Client } from '@elastic/elasticsearch';
-  
+import { PinataSDK } from "pinata-web3";
+import { readFile } from 'node:fs/promises';
+
+
+const pinata = new PinataSDK({
+  pinataJwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJmMzdjYmJiNi1kZjVhLTQ3M2UtOTk5ZC0yMTVmNDBkNGI1NzgiLCJlbWFpbCI6InFpYW5qaW5mZW5nQG91dGxvb2suY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiTllDMSJ9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjAzZmIzZDU2ZTVjYjhiNGI5OWQyIiwic2NvcGVkS2V5U2VjcmV0IjoiOTdkYmY0YTAwY2RmZDM5MjMyNTQwOTBhOGU4ODNjM2Q4ZjA3MDlhZDdmNGQ0OThhYzhhM2JlZGZmYTZiMzk0ZSIsImV4cCI6MTc1ODE5OTU2Nn0.uKOHEZEDlOp4Cv1KYva_f_nsxLzPNxr30mWTp4qXl68",
+  pinataGateway: "black-grubby-minnow-508.mypinata.cloud",
+});
+
+
 // RabbitMQ连接URL  
 const url = 'amqp://localhost';  
 const pipeline = 'dicoms@custom'
@@ -30,7 +39,18 @@ async function indexDocument(index, documentId, body) {
     } catch (error) {  
         console.error('Error indexing document', error);  
     }  
-}  
+}
+
+async function uploadFile(jsonBody) {
+    try {
+      
+      const uploadCID = await pinata.upload.json(jsonBody)
+      console.log(uploadCID);
+      return uploadCID;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
 // 异步函数来处理连接和接收消息  
 async function consumeMessages() {  
@@ -48,16 +68,30 @@ async function consumeMessages() {
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", 'hello');  
   
         // 订阅队列并接收消息  
-        channel.consume('dicom_queue', (msg) => {  
+        channel.consume('dicom_queue', async (msg) => {  
             if (msg !== null) {  
                 console.log(" [x] Received %s", msg.content.toString());  
                 // 注意：在真实应用中，你可能需要手动发送确认信号  
                 const adataset = JSON.parse(msg.content.toString());
                 const natural0 = DicomMetaDictionary.naturalizeDataset(adataset);
                 console.log(natural0.SOPInstanceUID);
+                
+
+                try {
+                    const filePath = new URL('/tmp/'+natural0.SOPInstanceUID, import.meta.url);
+                    const contents = await readFile(filePath, { encoding: 'utf8' });
+                    console.log(contents);
+
+                    const uploadCID = await pinata.upload.json(contents)
+                    natural0.PixelData.BulkDataURI = uploadCID.IpfsHash;
+
+                  } catch (err) {
+                    console.error(err.message);
+                }
+
                 console.log(JSON.stringify(natural0));
 
-                indexDocument('dicoms', natural0.SOPInstanceUID, natural0)
+                //indexDocument('dicoms', natural0.SOPInstanceUID, natural0)
             }  
         }, {  
             noAck: true // 自动确认消息  
